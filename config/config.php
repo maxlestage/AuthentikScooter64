59bd71a806170acd11e190cebe119998feccc790
maxlestage <maxlestage@icloud.com>
2020-03-16 17:27:04 +0100

    test for push heroku app

--- config/config.php
+++ config/config.php
@@ -10,11 +10,12 @@
  */
 
 
-define('APP_DEV', true);
+
+    define('APP_DEV', true);
 
 //Model (for connexion data, see unversionned db.php)
 //VIew
-define('APP_VIEW_PATH', __DIR__ . '/../src/View/');
-define('APP_CACHE_PATH', __DIR__ . '/../temp/cache/');
+    define('APP_VIEW_PATH', __DIR__ . '/../src/View/');
+    define('APP_CACHE_PATH', __DIR__ . '/../temp/cache/');
 
-define('HOME_PAGE', 'Home/index');
+    define('HOME_PAGE', 'Home/index');
--- public/index.php
+++ public/index.php
@@ -11,8 +11,6 @@
     }
 
 
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../config/debug.php';
-require_once __DIR__ . '/../config/db.php';
-require_once __DIR__ . '/../config/config.php';
-require_once __DIR__ . '/../src/routing.php';
+    require_once __DIR__ . '/../config/db.php';
+    require_once __DIR__ . '/../config/config.php';
+    require_once __DIR__ . '/../src/routing.php';
