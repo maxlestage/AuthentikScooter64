48b4f2aab7cfd4fb2dab13dde05827c44b6643f5
maxlestage <maxlestage@icloud.com>
2020-03-16 17:42:38 +0100

    test

--- config/config.php
+++ config/config.php
@@ -18,4 +18,5 @@
     define('APP_VIEW_PATH', __DIR__ . '/../src/View/');
     define('APP_CACHE_PATH', __DIR__ . '/../temp/cache/');
 
-    define('HOME_PAGE', 'Home/index');
+    define('HOME_PAGE', 'vhs/index');
+
